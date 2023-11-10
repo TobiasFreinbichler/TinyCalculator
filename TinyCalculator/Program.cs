469e118f3bc388803e049fdb5e31dab173981094
Freinbichler Tobias <Tobias.FREINBICHLER@htlvb.at>
2023-11-10 09:07:46 +0100

    Continue calculation until result is 1

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,32 +1,61 @@
-﻿Console.WriteLine("== Tiny calculator ==");
+﻿using System.Data;
 
-int dividend = ReadNumber("Dividend");
-int divisor = ReadNumber("Divisor");
+Console.WriteLine("Tiny Calculator");
+int firstNumber = 0;
 
-if (divisor == 0)
+firstNumber = GetNumbers(firstNumber, out int secondNumber);
+int Rechung = GetRechnung();
+Console.WriteLine($"{Rechung} and {firstNumber},{secondNumber} = {GetAnswer(Rechung, firstNumber, secondNumber)}");
+
+int Answer = GetAnswer(Rechung, firstNumber, secondNumber);
+if (CheckIfContinue())
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Divisor darf nicht 0 sein.");
-    Console.ResetColor();
+    Rechung = GetRechnung();
+    Console.WriteLine($"Your result is {GetAnswer(Rechung, Answer, GetAdditionalNum())}");
 }
-else
+static bool CheckIfContinue()
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
-    Console.ResetColor();
+    Console.Write("Do you want to continue with this result? (J/N):");
+    string s = Console.ReadLine();
+    if (s == "J") return true;
+    if (s == "N") return false;
+    return false;
 }
-static int ReadNumber(string name)
+
+
+static int GetRechnung()
 {
-    while (true)
-    {
-        Console.Write($"{name}: ");
-        string input = Console.ReadLine();
-        if (int.TryParse(input, out int dividend))
-        {
-            return dividend;
-        }
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"{input} is not a number.");
-        Console.ResetColor();
-    }
+    Console.Write("Wie möchtest du rechnen? (+ - * /):");
+    string z = Console.ReadLine();
+    if (z == "+") return 2;
+    if (z == "-") return 3;
+    if (z == "*") return 4;
+    if (z == ":") return 5;
+    return 2;
+}
+
+static int GetNumbers(int x, out int y)
+{
+    Console.Write("first Number: ");
+    x = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("second Number:");
+    y = Convert.ToInt32(Console.ReadLine());
+
+    return x;
+}
+static int GetAnswer(int R, int first, int second)
+{
+    int answer = 0;
+    if (R == 2 && first != null && second != null) return answer = first + second;
+    if (R == 3 && first != null && second != null) return answer = first - second;
+    if (R == 4 && first != null && second != null) return answer = first * second;
+    if (R == 5 && first != null && second != null) return answer = first * second;
+    return 0;
+}
+static int GetAdditionalNum()
+{
+    Console.Write("What is your Additional Number: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
 }
